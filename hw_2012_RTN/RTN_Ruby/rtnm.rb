d3efa3010c7e0d23dafc2a7891ab044cfd6b6d01
Mike Roylance <roylance.michael@gmail.com>
2012-09-06 20:05:45 -0600

    woot, reading all the models in!

--- hw_2012_RTN/RTN_Ruby/rtnm.rb
+++ hw_2012_RTN/RTN_Ruby/rtnm.rb
@@ -1,3 +1,4 @@
+require './rule.rb'
 class Rtnm
 	attr_accessor :machine, :init, :final, :rules
 	
@@ -5,21 +6,21 @@ class Rtnm
 		rawFile = File.new(filePath)
 		content = rawFile.read
 		propertyRegex = /MACHINE\s+([a-zA-Z0-9]+?)\s+INIT\s+([a-zA-Z0-9]+?)\s+FINAL\s+([a-zA-Z0-9\s]+?)\s+BEGIN([A-Za-z0-9\s]+?)END\s*/
-		match = propertyRegex.match(content)
 		machines = []
+		
+		match = propertyRegex.match(content)
 		while(match != nil)
-			postMatch = match.post_match.strip
-			
 			#puts "match: #{match}"
-			#puts "post match: #{postMatch}"
+			#puts "post match: #{match.post_match}"
 			
-			returnRtnm = Rtnm.new
-			returnRtnm.machine = match[1]
-			returnRtnm.init = match[2]
-			returnRtnm.final = match[3].strip.split(/\s/)
+			rtnm = Rtnm.new
+			rtnm.machine = match[1]
+			rtnm.init = match[2]
+			rtnm.final = match[3].strip.split(/\s/)
+			rtnm.rules = Rule.factory(match[4])
 			
-			machines.push returnRtnm
-			match = propertyRegex.match(postMatch)
+			machines.push rtnm
+			match = propertyRegex.match(match.post_match)
 		end
 		machines
 	end
--- hw_2012_RTN/RTN_Ruby/rule.rb
+++ hw_2012_RTN/RTN_Ruby/rule.rb
@@ -3,6 +3,18 @@ class Rule
 	
 	def self.factory(rawString)
 		#expecting to have \r\n delimiters
-	
+		propertyRegex = /^([A-Za-z0-9]+)\s([A-Za-z0-9]+)\s([A-Za-z0-9]+)$/
+		rules = []
+		match = propertyRegex.match(rawString)
+		
+		while match != nil		
+			rule = Rule.new
+			rule.start = match[1]
+			rule.arc = match[2]
+			rule.end = match[3]
+			rules.push rule
+			match = propertyRegex.match(match.post_match)
+		end
+		rules
 	end
 end
